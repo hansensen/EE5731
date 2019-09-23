2a83e4644d9bb57626b00705b6770ddfe7605320
Eugene Han <hanjie1030@gmail.com>
2019-09-23 21:21:57 +0800

    make key selection a function

--- Part3/part3.m
+++ Part3/part3.m
@@ -4,17 +4,7 @@ image1 = double(rgb2gray(imread('h1.jpg')));
 image2 = double(rgb2gray(imread('h2.jpg')));
 im = {image1, image2};
 
-image_size = length(im);
-
-keypoints = cell(1,2);
-% Get keypoints
-for i = 1 : image_size
-    figure;
-    imshow(im{i}, []);
-    % select 4 points from each images
-    keypoints(i) = {ginput(4)};
-    close;
-end
+keypoints = selectKeyPoints(im, 4);
 
 H = h_matrix(keypoints{1}, keypoints{2});
 
