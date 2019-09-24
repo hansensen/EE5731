db7d32cd74df9d52a1d58c954ade02477fdb94bf
Eugene Han <hanjie1030@gmail.com>
2019-09-24 13:44:14 +0800

    plot final panorama

--- Part5/part5.m
+++ Part5/part5.m
@@ -8,7 +8,7 @@ addpath('../Part2')
 image1 = double(rgb2gray(imread('../assg1/im01.jpg')));
 image2 = double(rgb2gray(imread('../assg1/im02.jpg')));
 images = {image1, image2};
-image_size = zeros(length(images),2);
+imageSize = zeros(length(images),2);
 keypoints = cell(1,length(images));
 
 keyPoints1 = SIFT(image1,3,5,1.3);
@@ -50,4 +50,61 @@ legend(ax, 'Matched points 1','Matched points 2');
 
 %% Step 3 - Get the Best Homography Matrix Using RANSAC
 
-%H_RANSAC = RANSAC(matchedKP1, matchedKP2, image1, image2);
+H = RANSAC(matchedKP1, matchedKP2, image1, image2);
+
+
+tforms(length(images)) = projective2d(eye(3));
+tforms(2) = projective2d(H.');
+
+%% Step 4 - Compute the output limits and create the corresponding panaroma
+
+% Compute the output limits  for each transform
+for i = 1:length(tforms)
+    % [xLimitsOut,yLimitsOut] = outputLimits(tform,xLimitsIn,yLimitsIn)
+    [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(i, 2)], [1 imageSize(i, 1)]);    
+end
+
+
+maxImageSize = max(imageSize);
+
+% Find the minimum and maximum output limits 
+xMin = min([1; xlim(:)]);
+xMax = max([maxImageSize(2); xlim(:)]);
+
+yMin = min([1; ylim(:)]);
+yMax = max([maxImageSize(1); ylim(:)]);
+
+% Width and height of panorama.
+width  = round(xMax - xMin);
+height = round(yMax - yMin);
+
+% Initialize the "empty" panorama.
+panorama = zeros([height width], 'like', images{1});
+
+% blender = vision.AlphaBlender('Operation', 'Binary mask', ...
+%     'MaskSource', 'Input port');  
+blender = vision.AlphaBlender;
+
+% Create a 2-D spatial reference object defining the size of the panorama
+xLimits = [xMin xMax];
+yLimits = [yMin yMax];
+panoramaView = imref2d([height width], xLimits, yLimits);
+
+% Create the panorama.
+for i = 1:length(images)
+    
+    I = images{i};
+   
+    % Transform I into the panorama.
+    warpedImage = imwarp(I, tforms(i), 'OutputView', panoramaView);
+                  
+    % Generate a binary mask.    
+    mask = imwarp(true(size(I,1),size(I,2)), tforms(i), 'OutputView', panoramaView);
+    
+    % Overlay the warpedImage onto the panorama.
+    % panorama = step(blender, panorama, warpedImage, mask);
+    panorama = step(blender, panorama, warpedImage);
+end
+
+figure
+imshow(panorama)
