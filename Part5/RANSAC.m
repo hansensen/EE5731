718c3b44de3b5eb5c57daac05a6d4c334d2619ce
Eugene Han <hanjie1030@gmail.com>
2019-09-29 20:15:08 +0800

    pre-filter potential H to accelerate

--- Part5/RANSAC.m
+++ Part5/RANSAC.m
@@ -1,37 +1,40 @@
 function bestH = RANSAC(matchedKP1,matchedKP2, image1, image2)   
     %% Determination of Inliers
     N_pts = length(matchedKP1); % total no. of points
-    distThreshold = 6;
+    distThreshold = 5;
     N_iter = 0;
     bestMatchedInliers = [];
     maxInliers = 0;
     currentError = 1.e1000;
-    while (maxInliers<60)
+    while (maxInliers < 80 && N_iter < 500000)
         N_iter = N_iter + 1;
         %% Random sample
         % Randomly Select 5 unique point pairs
-        randIndexes = getRandIndex(N_pts,9);
+        numPts = 5;
+        randIndexes = getRandIndex(N_pts,numPts);
         im1pts = matchedKP1(randIndexes);
         im2pts = matchedKP2(randIndexes);
 
-        im1ptsCo = zeros(5,2);
-        im2ptsCo = zeros(5,2);
-        for j = 1:5
+        im1ptsCo = zeros(numPts,2);
+        im2ptsCo = zeros(numPts,2);
+        for j = 1:numPts
             im1ptsCo(j,:) = fliplr(im1pts{j}.Coordinates);
             im2ptsCo(j,:) = fliplr(im2pts{j}.Coordinates);
         end
         %% Compute Homography Matrix H and Get Inliers
         H = h_matrix(im1ptsCo,im2ptsCo);
+        [numInliers, ~, ~] = getInliers(im1ptsCo, im2ptsCo, H, distThreshold);
+        if (numInliers == numPts)
+            % Count number of inliers using H
+            [matchedPointsIn1, matchedPointsIn2] = getCoordinates(matchedKP1, matchedKP2);
+            [numInliers, matchedInliers, error] = getInliers(matchedPointsIn1, matchedPointsIn2, H, distThreshold);
 
-        % Count number of inliers using H
-        [matchedPointsIn1, matchedPointsIn2] = getCoordinates(matchedKP1, matchedKP2);
-        [numInliers, matchedInliers, error] = getInliers(matchedPointsIn1, matchedPointsIn2, H, distThreshold);
-
-        if (numInliers >= maxInliers)
-            if (numInliers > maxInliers || error < currentError)
-                maxInliers = numInliers;
-                bestMatchedInliers = matchedInliers;
-                currentError = error;
+            if (numInliers >= maxInliers)
+                if (numInliers > maxInliers || error < currentError)
+                    maxInliers = numInliers;
+                    bestMatchedInliers = matchedInliers;
+                    currentError = error;
+                end
             end
         end
     end
