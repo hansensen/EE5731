9d0d43d53b3125e278df157de73a56ae22152d89
Eugene Han <hanjie1030@gmail.com>
2019-10-01 14:48:32 +0800

    refactor

--- Part5/getRandIndex.m
+++ Part5/getRandIndex.m
@@ -1,10 +1,10 @@
-function index = getRandIndex(maxIndex,numPts)
+function index = getRandIndex(maxIndex, numPts)
 % init index
 index = zeros(1,numPts);
 indexPool = 1:maxIndex;
 
 % Get random indexes in indexPool
-rs = ceil(rand(1,numPts).*(maxIndex:-1:maxIndex-numPts+1));
+rs = ceil(rand(1, numPts).*(maxIndex:-1:maxIndex-numPts+1));
 
 % Check 0
 for i = 1:numPts
--- Part5/getTform.m
+++ Part5/getTform.m
@@ -1,9 +1,9 @@
 function tform = getTform(image1, image2)
-    %% Step 1 - Load images and 
+    %% 1. Find the keypoints for both images
     keyPoints1 = SIFT(double(rgb2gray(image1)),3,5,1.6);
     keyPoints2 = SIFT(double(rgb2gray(image2)),3,5,1.6);
 
-    %% Step 2: Find the best matched points
+    %% 2. Find matched points
     matchedKP1 = keyPoints1;
     matchedKP2 = cell(length(keyPoints1));
 
@@ -22,13 +22,13 @@ function tform = getTform(image1, image2)
 
     [matchedPointsIn1, matchedPointsIn2] = getCoordinates(matchedKP1, matchedKP2);
 
-    %% Plot all the matches
+    % Plot all the matches
     figure; ax = axes;
-    showMatchedFeatures(image1,image2,matchedPointsIn1,matchedPointsIn2,'montage','Parent',ax);
+    showMatchedFeatures(image1, image2, matchedPointsIn1, matchedPointsIn2,'montage','Parent',ax);
     title(ax, 'Candidate point matches');
     legend(ax, 'Matched points 1','Matched points 2');
 
-    %% Step 3 - Get the Best Homography Matrix Using RANSAC
+    %% 3. Get the Best Homography Matrix Using RANSAC and tform matrix
     H = RANSAC(matchedKP1, matchedKP2, image1, image2);
 
     tform = projective2d(H.');
