4d8b18fe5bf37533a7daf3920a8b6906347b4400
Eugene Han <hanjie1030@gmail.com>
2019-10-01 13:55:09 +0800

    clean kernels

--- Part1/convoTest.m
+++ Part1/convoTest.m
@@ -3,11 +3,18 @@ rgbImage = imread('../assg1/im01.jpg');
 sobel_kernel = [-1 0 1; -2 0 2; -1 0 1];
 gaussian_kernel = [1 2 1; 2 4 2; 1 2 1];
 
-kernel = sobel(5);
+kernel1 = [1 -1;-1 1]
+kernel2 = [-1 1 -1]
+kernel3 = [-1; 1; -1]
+kernel4 = [-1 1]
+kernel5 = [-1; 1]
+kernel6 = [-1 -1 -1 -1 1 1 1 1; ...
+           -1 -1 -1 -1 1 1 1 1; ...
+           -1 -1 -1 -1 1 1 1 1; ...
+           -1 -1 -1 -1 1 1 1 1; ...
+           ]
 
-kernel = [1 -1;-1 1]
-kernel = [-1 1 -1]
-kernel = [-1; 1; -1]
+kernel = kernel6;
 
 image = convo(double(rgb2gray(rgbImage)), kernel);
 
