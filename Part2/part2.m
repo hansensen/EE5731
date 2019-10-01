bf903964cf6359005227072bdb040d31c8f9f1b7
Eugene Han <hanjie1030@gmail.com>
2019-10-01 16:54:23 +0800

    move files to assignment1

--- Part2/part2.m
+++
@@ -1,8 +0,0 @@
-
-image = imread('../assg1/im01.jpg');
-image = double(rgb2gray(image));
-
-keyPoints = SIFT(image,5,5,1.6);
-
-image = SIFTKeypointVisualizer(image,keyPoints);
-imshow(uint8(image));
