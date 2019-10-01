3dbd0c913de524b9efaea4fd8dd08984b4ef30da
Eugene Han <hanjie1030@gmail.com>
2019-10-01 13:55:28 +0800

    add h1 to h2

--- Part3/part3.m
+++ Part3/part3.m
@@ -2,6 +2,7 @@
 % Load images
 image1 = double(rgb2gray(imread('h1.jpg')));
 image2 = double(rgb2gray(imread('h2.jpg')));
+%% h2 to h1
 im = {image1, image2};
 
 keypoints = selectKeyPoints(im, 4);
@@ -11,3 +12,9 @@ H = h_matrix(keypoints{1}, keypoints{2});
 figure;
 tform = projective2d(H.');
 imshow(imwarp(image2, tform), [])
+
+H = h_matrix(keypoints{2}, keypoints{1});
+
+figure;
+tform = projective2d(H.');
+imshow(imwarp(image1, tform), [])
