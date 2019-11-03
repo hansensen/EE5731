20124256309efc8b3b8eaf8a56efae6af2c5cf58
Eugene Han <eugenehan@Eugenes-MacBook-Pro-2.local>
2019-11-03 16:37:52 +0800

    add part1

--- assignment2/Part1/getDistance.m
+++ assignment2/Part1/getDistance.m
@@ -1,5 +1,8 @@
 function dist = getDistance(c1, c2 )
 
+c1 = double(c1);
+c2 = double(c2);
+
 dist = (abs( c1(1) - c2(1) ) + ...
         abs( c1(2) - c2(2) ) + ...
         abs( c1(3) - c2(3) )) / 3;
--- assignment2/Part1/part1.m
+++ assignment2/Part1/part1.m
@@ -1,32 +1,110 @@
-int m_lambda = 200; 
-% change this value to change the weight of the smoothness or prior term
+%%
+addpath('../GCMex')
 
+m_lambda = 80;
 SOURCE_COLOR = [ 0, 0, 255 ]; % blue = foreground
-SINK_COLOR = [45, 210, 110 ]; % yellow = background
+SINK_COLOR = [245, 210, 110 ]; % yellow = background
+
+rgbImage = imread('./bayes_in.jpg');
+% Get the height and width of the image
+H = size(rgbImage, 1);
+W = size(rgbImage, 2);
+
+num_pixels = W * H;
+num_classes = 2;
+
+segclass = zeros(num_pixels, 1);
+unary = zeros(num_classes, num_pixels);
+[X Y] = meshgrid(1:num_classes, 1:num_classes);
+labelcost = min(4, (X - Y).*(X - Y));
+
+num_edges = H * (W -1) + W * (H - 1);
+i = zeros(1, num_edges);
+j = zeros(1, num_edges);
+adjacent = zeros(1,num_edges);
+edge_index = 1;
+% pairwise = sparse(num_pixels,num_pixels);
 
 
-for( int y = 0; y < img->height-1; y++ ) {
-    for( int x = 0; x < img->width-1; x++ ){
-
-    CvScalar c = cvGet2D( img, y, x );
-    node = y * img->width + x;
-
+%%
+for row = 0:H-1
+  for col = 0:W-1
+    pixel_index = 1+ row * W + col;
+    
     % data term:
-    graph->add_tweights(node,dist(SOURCE_COLOR,c),dist(SINK_COLOR,c));
-
+    pixel = reshape(rgbImage(row+1, col+1,:),[1,3]);
+    unary(2, pixel_index) = getDistance(pixel, SOURCE_COLOR);
+    unary(1, pixel_index) = getDistance(pixel, SINK_COLOR);
+    
     % prior term: start
+    currentPixel = reshape(rgbImage(row+1, col+1,:),[1,3]);
 
-    int nx = x + 1; % the right neighbor
-    int next_node = y*img->width+nx;
-    graph->add_edge(node, next_node, m_lambda, m_lambda );
+    if row+1 < H
+        i(edge_index) = pixel_index;
+        j(edge_index) = 1+col+(row+1)*W;
+        adjacent(edge_index) = 1;
+        edge_index = edge_index +1;
+    end
+    
+    if row-1 >= 0
+        i(edge_index) = pixel_index;
+        j(edge_index) = 1+col+(row-1)*W;
+        adjacent(edge_index) = 1;
+        edge_index = edge_index +1;
+    end
 
-    int ny = y + 1; % the below neighbor
-    next_node = ny*img->width+x;
-    graph->add_edge(node, next_node, m_lambda, m_lambda );
+    if col+1 < W
+        i(edge_index) = pixel_index;
+        j(edge_index) = 1+(col+1)+row*W;
+        adjacent(edge_index) = 1;
+        edge_index = edge_index +1;
+    end
+
+    if col-1 >= 0
+        i(edge_index) = pixel_index;
+        j(edge_index) = 1+(col-1)+row*W;
+        adjacent(edge_index) = 1;
+        edge_index = edge_index +1;
+    end
+    
+%     if row+1 < H, pairwise(pixel_index, 1+col+(row+1)*W) = 1; end
+%     if row-1 >= 0, pairwise(pixel_index, 1+col+(row-1)*W) = 1; end 
+%     if col+1 < W, pairwise(pixel_index, 1+(col+1)+row*W) = 1; end
+%     if col-1 >= 0, pairwise(pixel_index, 1+(col-1)+row*W) = 1; end 
 
     % prior term: end
+  end
+end
 
-    }
-}
+%%
+adjacent = adjacent *m_lambda;
+pairwise = sparse(i,j ,adjacent);
+[labels E Eafter] = GCMex(segclass, single(unary), pairwise, single(labelcost),1);
+fprintf('E: %d (should be 260), Eafter: %d (should be 44)\n', E, Eafter);
+fprintf('unique(labels) should be [0 4] and is: [');
+fprintf('%d ', unique(labels));
+fprintf(']\n');
 
-graph->maxflow();
+%%
+labels = reshape(labels, W, H)';
+
+final_img = [];
+SOURCE_COLOR = [ 0, 0, 255 ]; % blue = foreground
+SINK_COLOR = [245, 210, 110 ]; % yellow = background
+
+for row = 1:H
+  for col = 1:W
+      if (labels(row, col) == 1)
+          final_img(row, col, 1) = SOURCE_COLOR(1);
+          final_img(row, col, 2) = SOURCE_COLOR(2);
+          final_img(row, col, 3) = SOURCE_COLOR(3);
+      else
+          final_img(row, col,1) = SINK_COLOR(1);
+          final_img(row, col,2) = SINK_COLOR(2);
+          final_img(row, col,3) = SINK_COLOR(3);
+      end
+  end
+end
+
+figure;
+imshow(uint8(final_img))
