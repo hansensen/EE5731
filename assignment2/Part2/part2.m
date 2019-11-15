b319c3b8cc9f3b9bef31ebe674e17aad1cba1f18
hansensen <hanjie1030@gmail.com>
2019-11-15 16:29:05 +0800

    wrap up part2

--- assignment2/Part2/part2.m
+++ assignment2/Part2/part2.m
@@ -1,86 +1,87 @@
+
 %%
+clear
+clc
+
 addpath('../GCMex')
 addpath('../Part1')
 
-m_lambda = 1;
-
-rgbImage1 = imread('./im1_scaled.png');
-rgbImage2 = imread('./im2_scaled.png');
+tic
+rgbImage1 = double(imread('im2.png'));
+rgbImage2 = double(imread('im6.png'));
 
 % Get the height and width of the image
 H = size(rgbImage1, 1);
 W = size(rgbImage1, 2);
 
+% resize irgbImage1 and rgbImage2 to 1 * n * 3
+rgbImage1 = flip(rgbImage1 ,1);
+rgbImage1 = flip(rgbImage1,2);
+rgbImage1 = reshape(rgbImage1,1,[],3);
+
+rgbImage2 = flip(rgbImage2 ,1);
+rgbImage2 = flip(rgbImage2,2);
+rgbImage2 = reshape(rgbImage2,1,[],3);
+
 num_pixels = W * H;
-num_classes = W;
 
-segclass = zeros(num_pixels, 1);
-unary = zeros(num_classes, num_pixels);
-[X Y] = meshgrid(1:num_classes, 1:num_classes);
-labelcost = min(4, (X - Y).*(X - Y));
+% set disparity range
+disparity = 4:60;
 
-num_edges = H * (W -1) + W * (H - 1);
-i = zeros(1, num_edges);
-j = zeros(1, num_edges);
-adjacent = zeros(1,num_edges);
-edge_index = 1;
-% pairwise = sparse(num_pixels,num_pixels);
+% set classes
+segclass = disparity(1) * ones(1,num_pixels);
+
+threshold = 0.02*(disparity(end) - disparity(1));
+Labelcost = pdist2(disparity',disparity');
+Labelcost(Labelcost>threshold) = threshold;
+
+unary = [];
 
 %%
-for row = 0:H-1
-  for col = 0:W-1
-    pixel_index = 1+ row * W + col;
+for k= 1:length(disparity)
 
-    % data term:
-    pixel1 = reshape(rgbImage1(row+1, col+1,:),[1,3]);
-    for col2 = 1:W
-        pixel2 = reshape(rgbImage2(row+1, col2,:),[1,3]);
-        unary(col2, pixel_index) = getDistance(pixel1, pixel2);
-    end
-
-    % prior term: start
-%     currentPixel = reshape(rgbImage(row+1, col+1,:),[1,3]);
-% 
-%     if row+1 < H
-%         i(edge_index) = pixel_index;
-%         j(edge_index) = 1+col+(row+1)*W;
-%         adjacent(edge_index) = 1;
-%         edge_index = edge_index +1;
-%     end
-%
-%     if row-1 >= 0
-%         i(edge_index) = pixel_index;
-%         j(edge_index) = 1+col+(row-1)*W;
-%         adjacent(edge_index) = 1;
-%         edge_index = edge_index +1;
-%     end
-% 
-%     if col+1 < W
-%         i(edge_index) = pixel_index;
-%         j(edge_index) = 1+(col+1)+row*W;
-%         adjacent(edge_index) = 1;
-%         edge_index = edge_index +1;
-%     end
-% 
-%     if col-1 >= 0
-%         i(edge_index) = pixel_index;
-%         j(edge_index) = 1+(col-1)+row*W;
-%         adjacent(edge_index) = 1;
-%         edge_index = edge_index +1;
-%     end
-    % prior term: end
-  end
+    % For each disparity class, remove out of boundary term
+    % Then check L2 distance
+    t = disparity(k);
+    t = H*t;
+    img2 = rgbImage2;
+    img2(:,1:t,:) = [];
+    img1 = rgbImage1;
+    img1(:,(num_pixels-t+1):num_pixels,:) = [];
+    A = get_l2_dist(img1, img2);
+    A = [A 255*3*ones(1,t)];
+    unary = [unary;A];
 end
+%%
+% calculate the sparse matrix
+red_img = rgbImage1(:,:,1);
+green_img = rgbImage1(:,:,2);
+blue_img = rgbImage1(:,:,3);
+
+connected_pixels = get_connected_pixels(H, W);
+% get distances between two connected pixels
+src = connected_pixels(:,1);
+dist = connected_pixels(:,2);
+
+red_dist = sqrt((red_img(src)-red_img(dist)).^2);
+greeen_dist = sqrt((green_img(src)-green_img(dist)).^2);
+blue_dist = sqrt((blue_img(src)-blue_img(dist)).^2);
+dist_matrix = red_dist + greeen_dist + blue_dist;
+
+lambda = 0.005;
+
+pairwise = sparse(src, dist, double(dist_matrix),num_pixels, num_pixels, 4* num_pixels);
+unary = unary./max(unary(:));
 
 %%
-adjacent = adjacent *m_lambda;
-pairwise = sparse(i,j ,adjacent);
-[labels E Eafter] = GCMex(segclass, single(unary), pairwise, single(labelcost),1);
-fprintf('E: %d , Eafter: %d \n', E, Eafter);
-fprintf('unique(labels) should be [0 4] and is: [');
-fprintf('%d ', unique(labels));
-fprintf(']\n');
-%%
-
-figure;
-imshow(uint8(final_img))
+toc
+disp('Begin Optimization')
+tic
+[DispClass, Einit, Eafter] = GCMex(segclass, single(unary), lambda*pairwise, single(Labelcost));
+depth_map = disparity(DispClass+1);
+%Reshape to image size for display purpose
+result = mat2gray(reshape(depth_map, H, W));
+result = flip(result ,1);
+result = flip(result ,2);
+imshow(result)
+toc
