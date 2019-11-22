7734e0e74eadacc1b558ba61ee8eb102c4dd127d
hansensen <hanjie1030@gmail.com>
2019-11-23 00:02:51 +0800

    code cleaning

--- loadAndPlot.py
+++
@@ -1,5 +0,0 @@
-from skimage import io, viewer
-img = io.imread('image.jpg', as_gray=True)  # load the image as grayscale
-print('image matrix size:', img.shape)     # print the size of image
-print('\n First 5 columns and rows of the image matrix: \n', img[:5,:5]*255)
-viewer.ImageViewer(img).show()              # plot the image
